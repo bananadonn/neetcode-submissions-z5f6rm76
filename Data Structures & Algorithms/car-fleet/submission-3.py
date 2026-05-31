949f52ebc55085bcdc379220ba44a30259d2d3f9
Donald B. Reith <143947107+bananadonn@users.noreply.github.com>
2026-05-31 00:59:20 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        arr = []
+        for car, position in enumerate(position):
+            arr.append((position, car))
+        arr = sorted(arr)
+
+        print(arr)
+        final = {}
+        prevmax = 0
+        for i in range(len(arr) - 1, -1, -1):
+            epochs = (target - arr[i][0]) / speed[arr[i][1]]
+            prevmax = max(prevmax, epochs)
+            print("epoch:", epochs)
+            print("prevmin: ", prevmax)
+
+            final[prevmax] = 1 + final.get(epochs,0)
+        
+        print(final)
+
+        return len(final)
